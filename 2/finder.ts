3d3ec237ec7727f0725093b62346b0035e5ea374
Zac Zacal <Zac.Zacal@alaskaair.com>
2025-12-05 11:24:43 -0800

    Adds repeating at all finder

--- 2/finder.ts
+++ 2/finder.ts
@@ -1,17 +1,17 @@
-export function findInvalid(input: string): number[] {
+export function findInvalid(input: string, matcher: (x: string) => boolean): number[] {
   return input
     .split(",")
     .flatMap((range: string) => {
       const [from, to] = range.split("-");
-      return findRepeating(Number(from), Number(to));
+      return findRepeating(Number(from), Number(to), matcher);
     })
 }
 
-export function findRepeating(from: number, to: number): number[] {
+export function findRepeating(from: number, to: number, matcher: (x: string) => boolean): number[] {
   let repeating: number[] = []
   
   for(let x = from; x <= to; x++) {
-    if(isRepeating(x.toString())) {
+    if(isRepeatingTwice(x.toString())) {
       repeating.push(x);
     }
   }
@@ -19,12 +19,51 @@ export function findRepeating(from: number, to: number): number[] {
   return repeating;
 }
 
-export function isRepeating(x: string): boolean {
+export function isRepeatingTwice(x: string): boolean {
   if(x.length === 0 || x.length % 2 !== 0) {
     return false;
   }
   const a = x.slice(0, x.length/2);
   const b = x.slice(x.length/2);
 
-  return a === b;
+  return isRepeating([a,b]);
 }
+
+export function isRepeating<T>(x: T[]): boolean {
+  const set = new Set(x);
+  return set.size === 1;
+}
+
+export function isRepeatingAtAll(x: string): boolean {
+  if(x.length === 0 || x.length === 1) {
+    return false;
+  }
+
+  for(let i = 2; i <= x.length; i ++) {
+    if(x.length % i == 0) {
+      let chunks = [];
+      for(let j = 0; j < i; j++) {
+        chunks.push(x.slice(j*(x.length/i), (j+1)*(x.length/i)));
+      }
+      if(isRepeating(chunks)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+// starts
+// x.length = 6; i = 2; j = 0; x.length/i = 3; j*(x.length/i) = 0
+// x.length = 6; i = 2; j = 1; x.length/i = 3; j*(x.length/i) = 3
+// x.length = 6; i = 2; j = 2; x.length/i = 3; j*(x.length/i) = 6
+// x.length = 6; i = 2; j = 3; x.length/i = 3; j*(x.length/i) = 9
+// x.length = 6; i = 2; j = 4; x.length/i = 3; j*(x.length/i) = 12
+
+// ends
+// x.length = 6; i = 2; j = 0; x.length/i = 3;  : 3
+// x.length = 6; i = 2; j = 1; x.length/i = 3;  : 6
+// x.length = 6; i = 2; j = 2; x.length/i = 3;  : 6
+// x.length = 6; i = 2; j = 3; x.length/i = 3;  : 8
+// x.length = 6; i = 2; j = 4; x.length/i = 3;  : 10
