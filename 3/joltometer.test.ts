6bebb8f299dd741905590d5c44bdaa3bd174dbf0
Zac Zacal <Zac.Zacal@alaskaair.com>
2025-12-10 12:59:48 -0800

    Solves day 3 part 2

--- 3/joltometer.test.ts
+++ 3/joltometer.test.ts
@@ -1,5 +1,5 @@
 import { getInput } from "../inputs.js";
-import { findFirstMax, findLargetTwoDigitCombo } from "./joltometer.js";
+import { findFirstMax, findLargetTwoDigitCombo, findMaxCombo, findNDigitMax, numerify } from "./joltometer.js";
 import { describe, test, expect } from "vitest";
 
 describe("findFirstMax", () => {
@@ -14,6 +14,47 @@ describe("findLargetTwoDigitCombo", () => {
   });
 });
 
+describe("findMaxCombo", () => {
+  test("returns same when input length equals digits", () => {
+    expect(findMaxCombo([1,2,3], 3)).toEqual([1,2,3]);
+  });
+  test("finds largest n digit combo", () => {
+    const pool = [3, 4, 5, 1, 2, 3, 5];
+    const digits = 3;
+    expect(findMaxCombo(pool, digits)).toEqual([5,3,5]);
+  })
+
+  test("finds largest n digit combo when it is pool.shift", () => {
+    const pool = [3, 4, 5, 1, 2, 3, 5];
+    const digits = 6;
+    expect(findMaxCombo(pool, digits)).toEqual([4, 5, 1, 2, 3, 5]);
+  })
+
+  test("finds largest n digit combo when it is pool.unshift", () => {
+    const pool = [9, 8, 7, 6, 5, 4, 3];
+    const digits = 6;
+    expect(findMaxCombo(pool, digits)).toEqual([9, 8, 7, 6, 5, 4]);
+  })
+
+  test("finds largest n digit combo when it is first input from advent", () => {
+    const pool = [
+      2, 2, 3, 5, 3, 2, 4, 2, 2, 2, 2, 3, 2, 2, 4, 4, 3, 2, 2, 4, 2, 2, 3, 1, 2,
+      2, 3, 4, 2, 5, 1, 3, 3, 3, 3, 4, 3, 4, 2, 5, 2, 4, 3, 3, 6, 3, 4, 4, 3, 1,
+      5, 2, 2, 4, 4, 1, 1, 1, 1, 2, 2, 6, 3, 2, 3, 3, 6, 2, 4, 2, 2, 2, 5, 7, 4,
+      5, 4, 3, 3, 4, 5, 2, 4, 5, 2, 4, 5, 1, 3, 3, 2, 4, 4, 5, 5, 4, 6, 4, 4, 3,
+    ];
+    const digits = 12;
+    expect(findMaxCombo(pool, digits)).toEqual([ 7, 5, 5, 5, 5, 5, 5, 4, 6, 4, 4, 3 ]);
+  });
+  test("finds largest n digit combo when it is first sample from advent", () => {
+    const pool = [9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1];
+    const digits = 12;
+    expect(findMaxCombo(pool, digits)).toEqual([
+      9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1,
+    ]);
+  });
+});
+
 describe("adventofcode", () => {
   test("part 1", async () => {
     const input = await getInput(3);
@@ -24,5 +65,13 @@ describe("adventofcode", () => {
     expect(maxes).not.toBeNull();
     const sum = maxes.reduce((a,b) => a+b)
     expect(sum).toBe(17092);
+  });
+
+  test("part 2", async () => {
+    const input = await getInput(3);
+    const inputArr = input.split("\n").map(b => b.split("").map(Number));
+    const maxes = inputArr.map(findNDigitMax(12)).map(c => numerify(c));
+    const sum = maxes.reduce((a,b) => a+b)
+    expect(sum).toBe(170147128753455);
   });
 });
--- 3/joltometer.ts
+++ 3/joltometer.ts
@@ -17,3 +17,33 @@ export function findFirstMax(numbers: number[]) {
     return previousValue;
   }, [0,0]);
 }
+
+export function numerify(numbers: number[]) {
+  return Number(numbers.map(n => n.toString()).join(""))
+}
+
+export function findMaxCombo(numbers: number[], digits: number): number[] {
+  if(numbers.length <= digits) {
+    return numbers;
+  }
+  // pool = [3, 4, 5, 1, 2, 3, 5]; digits = 3;
+  // 0: pool.length = 7; digits = 3; poolEnd = 5; max = 5; pool = [1, 2, 3, 5]
+  // 1: pool.length = 7; digits = 3; poolEnd = 6; max = 3; pool = [5]
+  // 2: pool.length = 7; digits = 3; poolEnd = 7; max = 5; pool = []
+  // answer = [5,3,5]
+
+  let answer: number[] = [];
+  let genPool = numbers;
+
+  for(let i = 0; i < digits; i ++)
+  {
+    const poolEnd = genPool.length - digits + 1 + i;
+    const pool = genPool.slice(0, poolEnd);
+    const [max, maxi] = findFirstMax(pool);
+    answer.push(max);
+    genPool = genPool.slice(maxi + 1);
+  }
+  return answer;
+}
+
+export const findNDigitMax = (digits: number) => (numbers: number[]) => findMaxCombo(numbers, digits);
