08b0c9c1d50e6f7fb7237dbd01aeef02ae65b5fd
zzacal <jizacal@gmail.com>
2026-01-04 21:57:15 -0800

    Caches input to minimize server calls

--- inputs.ts
+++ inputs.ts
@@ -1,11 +1,33 @@
 import { promises as fs } from 'fs';
 
-export async function getSession(): Promise<string> {
+const dirPath = "./inputs"
+
+async function getSession(): Promise<string> {
   return await fs.readFile('session_id', 'utf-8');
 }
 
+async function makeInputDir() {
+  const dir = await fs.readdir(dirPath)
+  if(!dir) {
+    return await fs.mkdir(dirPath);
+  }
+}
 
-export async function getInput(day: number): Promise<string> {
+async function cache(day: number, input: string) {
+  await makeInputDir();
+  await fs.writeFile(`${dirPath}/${day}`, input);
+  return input;
+}
+
+async function getCached(day: number) {
+  try {
+    return await fs.readFile(`${dirPath}/${day}`, 'utf-8');  
+  } catch {
+    return null;
+  }
+}
+
+async function getNew(day: number) {
   const session = `session=${await getSession()}`;
   const response = await fetch(`https://adventofcode.com/2025/day/${day}/input`, {
     headers: {
@@ -13,4 +35,8 @@ export async function getInput(day: number): Promise<string> {
     },
   });
   return await response.text();
+}
+
+export async function getInput(day: number): Promise<string> {
+  return await getCached(day) ?? await cache(day, await getNew(day));
 }
